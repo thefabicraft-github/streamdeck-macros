6a48d0b2a7cb5df044ded9a8cb52360f1439ca9e
Fabian Neufang <fabian.neufang2007@gmail.com>
2020-05-31 09:21:19 +0200

    Added macro script to index.ts

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import * as express from 'express';
-import * as robot from 'robotjs';
+import * as ks from 'node-key-sender';
+import * as fs from 'fs';
 import functions = require("./functions.json");
 
 const app = express();
@@ -8,10 +9,11 @@ app.get("/", (req, res) => {
     res.json({ online: true });
 })
 
-app.get("/send/:function", (req, res) => {
+app.get("/send/:function", async (req, res) => {
     if (functions.includes(req.params.function)) {
-        robot.keyTap("audio_pause")
-        console.log("It worked!")
+        fs.writeFileSync('./key.txt', req.params.function)
+        ks.sendKey('f24');
+        res.send("OK")
     }
         else console.log("It didn't!")
 })
